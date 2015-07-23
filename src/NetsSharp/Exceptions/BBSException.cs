d6466ec21cd8033ea1ef8b2035a21c50883ebd34
Vegard Andreas Larsen <vegard@digitalcreations.no>
2015-07-23 08:38:47 +0200

    Better BBSException error message.

--- src/NetsSharp/Exceptions/BBSException.cs
+++ src/NetsSharp/Exceptions/BBSException.cs
@@ -7,8 +7,23 @@
     public class BBSException : Exception
     {
         public BBSException(XElement error)
-            : base(error.Descendants("Message").First().Value)
+            : base(FindMessage(error))
         {
+        }
+
+        private static string FindMessage(XElement error)
+        {
+            var message = error.Descendants("Message").First().Value;
+            if (error.Descendants("Result").Any())
+            {
+                var result = error.Descendants("Result").First();
+                if (result.Descendants("ResponseText").Any())
+                {
+                    message += ": " + result.Descendants("ResponseText").First().Value;
+                }
+            }
+
+            return message;
         }
     }
 }
